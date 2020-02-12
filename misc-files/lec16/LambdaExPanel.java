b15dff11d810766ba8b7a365ab22c69c603e9287
Erin Carrier <carrieer@gvsu.edu>
2020-02-12 09:55:10 -0500

    add file for today

---
+++ misc-files/lec16/LambdaExPanel.java
@@ -0,0 +1,55 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class LambdaExPanel extends JPanel {
+    private JButton b1;
+    private JButton b2;
+    private JButton b3;
+    private JLabel lab1;
+    private JLabel lab2;
+    private JLabel lab3;
+    private int count1;
+    private int count2;
+    private int count3;
+
+    public LambdaExPanel() {
+        count1 = 0;
+        count2 = 0;
+        count3 = 0;
+
+        b1 = new JButton("Click here 1");
+        b2 = new JButton("Click here 2");
+        b3 = new JButton("Click here 3");
+
+        b1.addActionListener(new ButtonListener());
+
+        add(b1);
+        add(b2);
+        add(b3);
+        lab1 = new JLabel("B1 pushes = " + count1);
+        lab2 = new JLabel("B2 pushes = " + count1);
+        lab3 = new JLabel("B3 pushes = " + count1);
+        add(lab1);
+        add(lab2);
+        add(lab3);
+    }
+
+    private class ButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            count1++;
+            lab1.setText("B1 pushes = " + count1);
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Lambda Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        LambdaExPanel panel = new LambdaExPanel();
+        panel.setLayout(new GridLayout(2,3));
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
